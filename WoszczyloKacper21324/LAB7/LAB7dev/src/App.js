f84c0d4f06e862a73cdbdc057384f846347f459c
kacprow21324 <21324@student.ans-elblag.pl>
2025-06-13 20:02:36 +0200

    LAB7

--- WoszczyloKacper21324/LAB7/LAB7dev/src/App.js
+++ WoszczyloKacper21324/LAB7/LAB7dev/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ToggleDetails from './ToggleDetails';
 import ScoreDisplay from './ScoreDisplay';
@@ -10,21 +9,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
+        <h1>Laboratorium 8</h1>
+        <h2>Zadanie 1.</h2>
         <ToggleDetails/>
 
+        <h2>Zadanie 2.</h2>
         <ScoreDisplay score={49}/>
         <ScoreDisplay score={51}/>
         <ScoreDisplay score={81}/>
 
+        <h2>Zadanie 3.</h2>
         <TaskList/>
 
+        <h2>Zadanie 4.</h2>
         <UserList/>
 
+        <h2>Zadanie 5.</h2>
         <TimerCounter/>
 
       </header>
